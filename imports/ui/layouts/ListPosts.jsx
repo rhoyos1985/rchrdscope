f1cd0a743011f7bd13028bf100c21894e5ddf877
Richard Hoyos <rchrdhysdev@gmail.com>
2017-06-01 10:49:36 -0500

    Look & Feel Posts

--- imports/ui/layouts/ListPosts.jsx
+++ imports/ui/layouts/ListPosts.jsx
@@ -7,9 +7,10 @@ import Post from '../components/Post.jsx'
 class ListPosts extends BaseComponent {
     getPosts() {
         return [
-            { _id: 1, img: 'img1', title: 'Titulo 1', user: 'Usuario 1', pubDate: '2017-05-31: 12:00:00 am', comments: '3', resumenPost: 'loreimpsum' },
-            { _id: 2, img: 'img2', title: 'Titulo 2', user: 'Usuario 2', pubDate: '2017-05-31: 12:00:00 am', comments: '3', resumenPost: 'loreimpsum' },
-            { _id: 3, img: 'img3', title: 'Titulo 3', user: 'Usuario 3', pubDate: '2017-05-31: 12:00:00 am', comments: '3', resumenPost: 'loreimpsum' },
+            { _id: 1, img: '/img/cod.png', title: 'Titulo 1', user: 'Usuario 1', pubDate: '12:00:00 am', comments: '3', resumenPost: 'loreimpsum' },
+            { _id: 2, img: '/img/cod.png', title: 'Titulo 2', user: 'Usuario 2', pubDate: '12:00:00 am', comments: '3', resumenPost: 'loreimpsum' },
+            { _id: 3, img: '/img/cod.png', title: 'Titulo 3', user: 'Usuario 3', pubDate: '12:00:00 am', comments: '3', resumenPost: 'loreimpsum' },
+            { _id: 4, img: '/img/cod.png', title: 'Titulo 3', user: 'Usuario 3', pubDate: '12:00:00 am', comments: '3', resumenPost: 'loreimpsum' },
         ];
     }
 
@@ -22,9 +23,9 @@ class ListPosts extends BaseComponent {
     render() {
         return (
             <div className="content-post">
-                <ul>
+                <div className="row content_post_list">
                     {this.renderPosts()}
-                </ul>
+                </div>
             </div>
         );
     }
